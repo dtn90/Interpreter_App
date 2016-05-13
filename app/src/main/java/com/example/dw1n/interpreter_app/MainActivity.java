776a9a7e7e2a444595ac12c63bbf3306d216090c
Daniel <dan.win@gmail.com>
2016-05-13 16:51:32 -0700

    Set up input onClickListener

--- app/src/main/java/com/example/dw1n/interpreter_app/MainActivity.java
+++ app/src/main/java/com/example/dw1n/interpreter_app/MainActivity.java
@@ -4,7 +4,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView error = (TextView) findViewById(R.id.view_error);
 
         Button clear_button = (Button) findViewById(R.id.button_clear);
         clear_button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                shapeLayout.removeAllViews();
+            public void onClick(View view) {
+                error.setText("");
+
+            }
+        });
+
+        Button compile_button = (Button) findViewById(R.id.button_compile);
+        compile_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText input = (EditText) findViewById(R.id.view_input);
+
+                error.setText(input.getText());
             }
         });
     }
 }
+
