5f581ce4476470d5cd0fc21088a390cfca853e9f
Daniel <dan.win@gmail.com>
2016-05-13 15:49:00 -0700

    Set up Clear Button

--- app/src/main/java/com/example/dw1n/interpreter_app/MainActivity.java
+++ app/src/main/java/com/example/dw1n/interpreter_app/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.dw1n.interpreter_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout shapeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button clear_button = (Button) findViewById(R.id.button_clear);
+        clear_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shapeLayout.removeAllViews();
+            }
+        });
     }
 }
